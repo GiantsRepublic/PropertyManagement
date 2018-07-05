ccf8498bf09876dc6a845a25bacf9817fc8553e5
GiantsRepublic <jason.wei.kf@gmail.com>
2018-07-05 15:36:58 +0800

    complete create and show

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -4,7 +4,19 @@ class PropertiesController < ApplicationController
   end
 
   def create
-    render plain: params[:property].inspect
+    @property = Property.new(property_params)
+
+    @property.save
+    redirect_to @property
+  end
+
+  def show
+    @property = Property.find(params[:id])
   end
 
 end
+
+private
+  def property_params
+    params.require(:property).permit(:name, :date, :user, :price, :status)
+  end
